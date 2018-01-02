b62b9bd8f3160a7d757bd466f248387cc0a57595
thomsc <thomsc@HUD-006741>
2018-01-02 14:24:39 -0500

    Add project files.

---
+++ OFE Populate Pass/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace OFE_Populate_Pass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SetupButton = new System.Windows.Forms.Button();
+            this.ExtractButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SetupSheetLabel = new System.Windows.Forms.Label();
+            this.ExtractStatusLabel = new System.Windows.Forms.Label();
+            this.status_lbl = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ReportLabel = new System.Windows.Forms.Label();
+            this.ReportButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SetupButton
+            // 
+            this.SetupButton.Location = new System.Drawing.Point(483, 42);
+            this.SetupButton.Name = "SetupButton";
+            this.SetupButton.Size = new System.Drawing.Size(75, 23);
+            this.SetupButton.TabIndex = 1;
+            this.SetupButton.Text = "Setup Sheet";
+            this.SetupButton.UseVisualStyleBackColor = true;
+            this.SetupButton.Click += new System.EventHandler(this.SetupButton_Click);
+            // 
+            // ExtractButton
+            // 
+            this.ExtractButton.Location = new System.Drawing.Point(483, 123);
+            this.ExtractButton.Name = "ExtractButton";
+            this.ExtractButton.Size = new System.Drawing.Size(75, 23);
+            this.ExtractButton.TabIndex = 3;
+            this.ExtractButton.Text = "Extract";
+            this.ExtractButton.UseVisualStyleBackColor = true;
+            this.ExtractButton.Click += new System.EventHandler(this.ExtractButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(483, 181);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 5;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // SetupSheetLabel
+            // 
+            this.SetupSheetLabel.AutoEllipsis = true;
+            this.SetupSheetLabel.Location = new System.Drawing.Point(11, 16);
+            this.SetupSheetLabel.Name = "SetupSheetLabel";
+            this.SetupSheetLabel.Size = new System.Drawing.Size(461, 23);
+            this.SetupSheetLabel.TabIndex = 4;
+            // 
+            // ExtractStatusLabel
+            // 
+            this.ExtractStatusLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExtractStatusLabel.Font = new System.Drawing.Font("Baskerville Old Face", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExtractStatusLabel.ForeColor = System.Drawing.Color.Red;
+            this.ExtractStatusLabel.Location = new System.Drawing.Point(186, 0);
+            this.ExtractStatusLabel.Name = "ExtractStatusLabel";
+            this.ExtractStatusLabel.Size = new System.Drawing.Size(258, 23);
+            this.ExtractStatusLabel.TabIndex = 6;
+            this.ExtractStatusLabel.Text = "Waiting for input files...";
+            this.ExtractStatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // status_lbl
+            // 
+            this.status_lbl.Font = new System.Drawing.Font("Baskerville Old Face", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.status_lbl.Location = new System.Drawing.Point(124, 0);
+            this.status_lbl.Name = "status_lbl";
+            this.status_lbl.Size = new System.Drawing.Size(73, 23);
+            this.status_lbl.TabIndex = 7;
+            this.status_lbl.Text = "Status:     ";
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(483, 152);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.SetupSheetLabel);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Location = new System.Drawing.Point(2, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(566, 43);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // ReportLabel
+            // 
+            this.ReportLabel.AutoEllipsis = true;
+            this.ReportLabel.Location = new System.Drawing.Point(11, 16);
+            this.ReportLabel.Name = "ReportLabel";
+            this.ReportLabel.Size = new System.Drawing.Size(461, 23);
+            this.ReportLabel.TabIndex = 5;
+            // 
+            // ReportButton
+            // 
+            this.ReportButton.Location = new System.Drawing.Point(483, 85);
+            this.ReportButton.Name = "ReportButton";
+            this.ReportButton.Size = new System.Drawing.Size(75, 23);
+            this.ReportButton.TabIndex = 2;
+            this.ReportButton.Text = "OFE Report";
+            this.ReportButton.UseVisualStyleBackColor = true;
+            this.ReportButton.Click += new System.EventHandler(this.ReportButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ReportLabel);
+            this.groupBox2.Location = new System.Drawing.Point(2, 71);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(566, 46);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(570, 205);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.status_lbl);
+            this.Controls.Add(this.ExtractStatusLabel);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.ExtractButton);
+            this.Controls.Add(this.ReportButton);
+            this.Controls.Add(this.SetupButton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "OFE Populate Pass";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SetupButton;
+        private System.Windows.Forms.Button ExtractButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Label SetupSheetLabel;
+        private System.Windows.Forms.Label ExtractStatusLabel;
+        private System.Windows.Forms.Label status_lbl;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label ReportLabel;
+        private System.Windows.Forms.Button ReportButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ OFE Populate Pass/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace OFE_Populate_Pass
+{
+    public partial class Form1 : Form
+    {
+        private List<string> fileLines { get; set; }
+        public object[,] ValueArray { get; set; }
+
+        public Dictionary<string, string> rdHash;
+        public Form1()
+        {
+            InitializeComponent();
+            fileLines = new List<string>();
+            rdHash = new Dictionary<string, string>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ExtractButton_Click(object sender, EventArgs e)
+        {
+            if(rdHash.Count > 0)
+            {
+                rdHash.Clear();
+            }
+            if(fileLines.Count > 0 && ValueArray.Length > 0)
+            {
+                PopulateRdDictionary();
+                LookupRefdes();
+                CreateOutputFile();
+                ExtractStatusLabel.Text = "Extraction Complete.";
+                ExtractStatusLabel.ForeColor = Color.Green;
+            }
+            else
+            {
+                MessageBox.Show("One or more input files was not processed correctly.\nPlease check your input files and try again.", "File Read Error");
+            }
+        }
+
+        private void LookupRefdes()
+        {
+            string value = null;
+            for (int i = 1; i < ValueArray.GetLength(0) + 1; i++)
+            {
+                if (rdHash.TryGetValue(ValueArray[i, 1].ToString(), out value))
+                {
+                    ValueArray[i, 4] = value;
+                }
+                else
+                {
+                    ValueArray[i, 4] = "";
+                }
+            }
+        }
+
+        private bool IsValidOFE()
+        {
+            bool isValid = false;
+            if(ValueArray != null && ValueArray.GetLength(0) >= 4)
+            {
+                if (ValueArray[1, 1].ToString().Equals("refdes") && ValueArray[1, 3].ToString().Equals("npins"))
+                    isValid = true;
+            }
+            return isValid;
+        }
+
+        private void CreateOutputFile()
+        {
+            Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+            Excel.Workbook m_ExpBook = app.Workbooks.Add(Excel.XlWBATemplate.xlWBATWorksheet);
+            Excel.Worksheet exportSheet = m_ExpBook.Worksheets[1];
+            try
+            {
+                exportSheet.Cells.NumberFormat = "@";
+                //copy to new Excel workbook and prompt user to save
+                Excel.Range firstcell = exportSheet.Cells[1, 1];
+                Excel.Range lastcell = exportSheet.Cells[ValueArray.GetLength(0), ValueArray.GetLength(1)];
+                Excel.Range targetrange = exportSheet.Range[firstcell, lastcell];
+                targetrange.Value = ValueArray;
+
+                // Clean up.
+                m_ExpBook.Close(true);
+                Marshal.ReleaseComObject(m_ExpBook);
+            }
+            catch (Exception ex)
+            {
+                m_ExpBook.Close(true);
+                Marshal.ReleaseComObject(m_ExpBook);
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+        }
+        private void PopulateRdDictionary()
+        {
+            Regex reEnd = new Regex("REFERENCE DESIGNATOR COUNT");
+            Regex reContainsPassInfo = new Regex(@"Program:.*Date:.*Side:\s+(\b.*\b)");
+            Regex reNoRefs = new Regex("No Ref Des|SMT 1|SMT 2");
+            Regex rePartNumber = new Regex(@"\\par \S+\\tab");
+            string pass = null;
+            bool bGetRds = false;
+
+            for(var i = 0; i < fileLines.Count; i++)
+            {
+                string line = fileLines[i];
+                if (reEnd.Match(line).Success)
+                    return;
+                if (bGetRds)
+                {
+                    if (reNoRefs.Match(line).Success)
+                        continue;
+                    PopInitialRds(line, pass);
+                    bGetRds = false;
+                    ++i;
+                    PopAdditionalRds(i, pass);
+                    continue;
+                }                
+
+                Match matchpass = reContainsPassInfo.Match(line);
+                if (matchpass.Success)
+                {
+                    pass = matchpass.Groups[1].Captures[0].Value;
+                    continue;
+                }
+
+                if (rePartNumber.Match(line).Success)
+                    bGetRds = true;
+            }
+        }
+
+        private void PopInitialRds(string line, string pass)
+        {
+            string[] separator = new string[] { "\\tab " };
+            char[] commasep = new char[] { ',' };
+            string[] results = line.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            if(results.Length > 3)
+            {
+                string[] temprds = results[3].Trim().Split(commasep, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string refdes in temprds)
+                {
+                    rdHash.Add(refdes, pass);
+                }
+            }      
+        }
+
+        private void PopAdditionalRds(int index, string pass)
+        {
+            Regex reEnd = new Regex("REFERENCE DESIGNATOR COUNT");
+            Regex rePn = new Regex(@"\A\\par \S*\\tab");
+            string[] tabsep = new string[] { "\\tab \\tab \\tab " };
+            string[] commasep = new string[] { "," };
+            for (var i = index; i < fileLines.Count; i++)
+            {
+                if (reEnd.Match(fileLines[i]).Success)
+                    return;
+                if (!fileLines[i].Contains("\\tab \\tab \\tab ") && rePn.Match(fileLines[i]).Success && !fileLines[i].Contains(@"BOM") && !fileLines[i].Contains(@"Listing Date"))
+                    return;
+                string[] refs = fileLines[i].Split(tabsep, StringSplitOptions.None);
+                if(refs.Length > 1)
+                {
+                    refs[1] = refs[1].Trim();
+                    string[] temprds = refs[1].Split(commasep, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (string refdes in temprds)
+                    {
+                        rdHash.Add(refdes, pass);
+                    }
+                }
+            }
+
+        }
+        private void SetupButton_Click(object sender, EventArgs e)
+        {     
+            OpenFileDialog fileDlg = new OpenFileDialog();
+            fileDlg.Title = "Load the Setup Sheet";
+            fileDlg.Filter = "Setup Sheet (*.rtf)|*.rtf";
+            fileDlg.RestoreDirectory = true;
+
+            if (fileDlg.ShowDialog() == DialogResult.OK)
+            {
+                SetupSheetLabel.ResetText();
+                fileLines.Clear();
+                string line = null;
+                try
+                {
+                    using(StreamReader reader = new StreamReader(fileDlg.FileName))
+                    {
+                        while( (line = reader.ReadLine()) != null)
+                        {
+                            fileLines.Add(line);
+                        }
+                    }
+                  
+                    SetupSheetLabel.Text = fileDlg.FileName;
+
+                    if (ReportLabel.Text.Length > 4)
+                    {
+                        ExtractStatusLabel.Text = "Ready to extract...";
+                        ExtractStatusLabel.ForeColor = Color.DarkKhaki; 
+                    }
+                    else
+                    {
+                        ExtractStatusLabel.Text = "Waiting for input files...";
+                        ExtractStatusLabel.ForeColor = Color.Red;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+            fileDlg.Dispose();
+        }
+
+        private void ReportButton_Click(object sender, EventArgs e)
+        {          
+            OpenFileDialog fileDlg = new OpenFileDialog();
+            fileDlg.Title = "Load the OFE Report";
+            fileDlg.Filter = "OFE Report|*.xls;*.xlsx" ;
+            fileDlg.RestoreDirectory = true;
+
+            if (fileDlg.ShowDialog() == DialogResult.OK)
+            {
+                ReportLabel.ResetText();
+                if(ValueArray != null)
+                {
+                    Array.Clear(ValueArray, 1, ValueArray.Length);
+                }         
+                Excel.Application xlApp = new Excel.Application();
+                Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(fileDlg.FileName,
+                        Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                        Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                        Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                        Type.Missing, Type.Missing);
+                try
+                {
+                    Excel.Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+                    Excel.Range xlRange = xlWorksheet.UsedRange;
+                    ValueArray = xlRange.get_Value(Excel.XlRangeValueDataType.xlRangeValueDefault);
+
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+
+                    xlWorkbook.Close();
+                    Marshal.ReleaseComObject(xlWorkbook);
+
+                    xlApp.Quit();
+                    Marshal.ReleaseComObject(xlApp);
+                    if (IsValidOFE())
+                    {
+                        ReportLabel.Text = fileDlg.FileName;
+                        if (SetupSheetLabel.Text.Length > 4)
+                        {
+                            ExtractStatusLabel.Text = "Ready to extract...";
+                            ExtractStatusLabel.ForeColor = Color.DarkKhaki;
+                        }
+                        else
+                        {
+                            ExtractStatusLabel.Text = "Waiting for input files...";
+                            ExtractStatusLabel.ForeColor = Color.Red;
+                        }
+                    }
+                    else
+                    {
+                        if (ValueArray != null)
+                        {
+                            Array.Clear(ValueArray, 1, ValueArray.Length);
+                        }
+                        MessageBox.Show("The OFE Report does not appear to be in a valid format.\nPlease check the file and try again", "Invalid OFE Report Format");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    xlWorkbook.Close();
+                    Marshal.ReleaseComObject(xlWorkbook);
+                    xlApp.Quit();
+                    Marshal.ReleaseComObject(xlApp);
+                    if(ValueArray != null)
+                    {
+                        Array.Clear(ValueArray, 1, ValueArray.Length);
+                    }
+                    MessageBox.Show("Problem loading OFE Report. Please check the file and try again.\n" + ex.Message, "ReportButton_Click()");
+                }
+            }
+            fileDlg.Dispose();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            fileLines.Clear();
+            rdHash.Clear();
+            if(ValueArray != null && ValueArray.Length > 0)
+            {
+                Array.Clear(ValueArray, 1, ValueArray.Length);
+            }
+            ExtractStatusLabel.Text = "Waiting for input files...";
+            ExtractStatusLabel.ForeColor = Color.Red;
+            SetupSheetLabel.ResetText();
+            ReportLabel.ResetText();
+        }
+    }
+}
